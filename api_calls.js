6374267d09ddd0184e02c22cfdab990bdcd769dd
BaneleMlamleli <mlamlelibanele@yahoo.com>
2022-06-27 16:55:49 +0200

    Initial commit

---
+++ api_calls.js
@@ -0,0 +1,3 @@
+console.log("Test");
+// https://www.learnhowtoprogram.com/javascript-part-time-react-track/asynchrony-and-apis-81b92804-5afe-4d1a-8209-5d9a3d3524e1/making-api-calls-with-javascript
+// https://www.youtube.com/watch?v=RG-weA9HUrg
