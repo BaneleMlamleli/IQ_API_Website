f9f3c46ccf1f3225e77b86fb616648808c8b5755
BaneleMlamleli <mlamlelibanele@yahoo.com>
2022-07-03 04:48:11 +0200

    Added Jumbutro, navigation tabs and a styling file

--- api_calls.js
+++ api_calls.js
@@ -1,3 +1,8 @@
 console.log("Test");
 // https://www.learnhowtoprogram.com/javascript-part-time-react-track/asynchrony-and-apis-81b92804-5afe-4d1a-8209-5d9a3d3524e1/making-api-calls-with-javascript
 // https://www.youtube.com/watch?v=RG-weA9HUrg
+
+$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+    e.target // newly activated tab
+    e.relatedTarget // previous active tab
+  })
